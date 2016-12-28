66b2b50f4971223f3722bb1c1b817de4a15b833c
dalek7 <dalek@kaist.ac.kr>
2016-12-29 07:30:37 +0900

    added anoter g2otest project (Code::Blocks)

---
+++ Optimization/g2o/g2otest/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+#include <Eigen/Core>
+#include <Eigen/StdVector>
+
+#include "g2o/types/slam3d/vertex_se3.h"
+#include "g2o/types/slam3d/edge_se3.h"
+#include "g2o/stuff/sampler.h"
+#include "g2o/stuff/command_args.h"
+#include "g2o/core/factory.h"
+
+using namespace std;
+using namespace g2o;
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
