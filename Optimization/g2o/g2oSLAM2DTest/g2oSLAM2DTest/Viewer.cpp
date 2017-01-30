f7d4c300315f40cf4a81ac5106a8979ee771124c
dalek7 <dalek@kaist.ac.kr>
2017-01-30 17:19:38 +0900

    initial commit

---
+++ Optimization/g2o/g2oSLAM2DTest/g2oSLAM2DTest/Viewer.cpp
@@ -0,0 +1,9 @@
+//
+//  Viewer.cpp
+//  g2oSLAM2DTest
+//
+//  Created by Seung-Chan Kim on 1/30/17.
+//  Copyright (c) 2017 Seung-Chan Kim. All rights reserved.
+//
+
+#include "Viewer.h"
---
+++ Optimization/g2o/g2oSLAM2DTest/g2oSLAM2DTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  g2oSLAM2DTest
+//
+//  Created by Seung-Chan Kim on 1/30/17.
+//  Copyright (c) 2017 Seung-Chan Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
